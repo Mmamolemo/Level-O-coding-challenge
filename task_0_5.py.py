96cfe176a8d8a6b789fc7cc4b456d56c0cc9c878
Mmamolemo <83614011+Mmamolemo@users.noreply.github.com>
2022-05-19 20:02:48 +0200

    deleted replica

--- task_0_5.py.py
+++
@@ -1,6 +0,0 @@
-
-def area_of_traingle(side1,side2,side3):
-     semiperemeter = (side1+side2+side3)/2
-     area = (semiperemeter*(semiperemeter-side1)*(semiperemeter-side2)*(semiperemeter-side3)) ** 0.5
-     return area
-
