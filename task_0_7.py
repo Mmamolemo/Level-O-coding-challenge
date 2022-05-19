888ed82d77efece29163670835a03aade33d91f1
Mmamolemo <83614011+Mmamolemo@users.noreply.github.com>
2022-05-19 18:16:05 +0200

    changes requested

---
+++ task_0_7.py
@@ -0,0 +1,8 @@
+def temperature_fahrenheit(celsius):
+    fahrenheit = (celsius * 1.8) + 32.0
+    print(fahrenheit)
+    
+    
+def temperature_celsius(fahrenheit):
+    celsuis = (fahrenheit - 32) * 5.0/9.0
+    print(celsuis)
