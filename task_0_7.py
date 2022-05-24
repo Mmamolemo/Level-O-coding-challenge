0365dee632fda46ef9982d31b98d6384c229979a
Mmamolemo <83614011+Mmamolemo@users.noreply.github.com>
2022-05-24 09:24:46 +0200

    changes

--- task_0_7.py
+++ task_0_7.py
@@ -1,12 +1,8 @@
-def temperature_fahrenheit(celsius):
-    fahrenheit = (celsius * 1.8) + 32.0
+def temperature_fah(celsius_convert_to_fahrenheit):
+    fahrenheit = (celsius_convert_to_fahrenheit * 1.8) + 32.0
     return(fahrenheit)
     
     
-def temperature_celsuis(fahrenheit):
-    celsuis = (fahrenheit - 32) * 5.0/9.0
-<<<<<<< HEAD:task_0_7.py
-    print(celsuis)
-=======
+def temperature_cels(fahrenheit_convert_to_celsuis):
+    celsuis = (fahrenheit_convert_to_celsuis - 32) * 5.0/9.0
     return(celsuis)
->>>>>>> d1135256ff05595364dfcec01519e2d5a34cc4c3:task-0_7.py.py
