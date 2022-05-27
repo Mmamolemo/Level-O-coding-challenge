6427b1fb0b4e3aa6680a4cbd446d4d9aeae5ae51
Mmamolemo <83614011+Mmamolemo@users.noreply.github.com>
2022-05-27 12:36:16 +0200

    changes

--- task_0_7.py
+++ task_0_7.py
@@ -1,9 +1,9 @@
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit-32)*5/9
-    
-    return(print("degrees in celsuis:", celsuis))
+    print("degrees in celsuis")
+    return celsius
 
 def celsius_to_fahrenheit(celsius):
-    fahrenheit = (celsuis * 1.8) = 32.0
-    
-    return(print("degrees in fahrenheit", fahrenheit))
+    fahrenheit = (celsius * 1.8) + 32.0
+    print("degrees in fahrenheit")
+    return fahrenheit
