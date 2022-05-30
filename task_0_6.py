f4bca19219fa75e1a2e2223dbb853ca2ac6d15ed
Mmamolemo <83614011+Mmamolemo@users.noreply.github.com>
2022-05-30 08:53:56 +0200

    account for negative numbers

--- task_0_6.py
+++ task_0_6.py
@@ -2,7 +2,8 @@ def maximum_number(num1,num2,num3):
     
     number_list = [num1, num2, num3]
     
-    max_num = 0
+    max_num = max_num = number_list[0]
+
     
     for number in number_list:
         if number > max_num:
