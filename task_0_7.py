0b1007080acdae60ba961d4d22bfe4c7dbbb5a09
Mmamolemo <83614011+Mmamolemo@users.noreply.github.com>
2022-05-30 08:03:27 +0200

    removed print statement

--- task_0_7.py
+++ task_0_7.py
@@ -1,9 +1,7 @@
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit-32)*5/9
-    print("degrees in celsuis")
     return celsius
 
 def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * 1.8) + 32.0
-    print("degrees in fahrenheit")
     return fahrenheit
