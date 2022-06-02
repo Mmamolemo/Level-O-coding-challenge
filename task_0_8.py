83895a84048b05515cd428d9fdb262760a0f96b0
Mmamolemo <83614011+Mmamolemo@users.noreply.github.com>
2022-06-02 12:22:05 +0200

    changes

--- task_0_8.py
+++ task_0_8.py
@@ -6,9 +6,9 @@ def time(nums):
     if number < 60:
         minutes = number
         if minutes == 1:
-            print("0 hours",minutes, 'minute')
+            print(f"0 hours, {minutes} minute")
         elif (minutes == 0 or minutes > 1):
-            print("0 hours",minutes, 'minutes')
+            print(f"0 hours, {minutes} minutes")
         
             
     elif number > 59:
@@ -16,13 +16,14 @@ def time(nums):
         minutes = number - (hours * 60)
         
         if hours == 1 and minutes == 0:
-            print(hours, 'hour,', minutes,'minutes')
+            print(f'{hours} hour, {minutes} minutes')
         elif hours == 1 and minutes == 1:
-            print(hours, 'hour,', minutes,'minute')
+            print(f'{hours} hour, {minutes} minute')
         elif hours > 1 and minutes == 1:
-            print(hours, 'hours,', minutes,'minute')
+           print(f'{hours} hour, {minutes} minute')
         elif hours == 1 and minutes > 1:
-            print(hours, 'hour,', minutes,'minutes')
+            print(f'{hours} hour, {minutes} minutes')
         else:
-            print(hours, 'hours,', minutes,'minutes')
+           print(f'{hours} hour, {minutes} minutes')
+
 
